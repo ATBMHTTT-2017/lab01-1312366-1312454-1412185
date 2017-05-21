2c6ce7ae930772bccf07d0410eed4685baae90cc
tnminh <minhtn@hotmail.com.vn>
2017-05-21 23:26:23 +0700

    Update

--- VPD_04_1312366.sql
+++ VPD_04_1312366.sql
@@ -11,10 +11,11 @@ as
   as
     flag number;
   begin
+    dbms_session.set_context('NhanVien_context','KiemTraNhanVienKhongGiuChucVu',2);
     flag:=0;
     select  count(N.MANV)
     into flag
-    from BT1N28.NHANVIEN_366_454_185 N, BT1N28.PHONGBAN_366_454_185 P, BT1N28.DUAN_366_454_185 D,BT1N28.CHINHANH_366_454_185 CN
+    from BT1N28.NHANVIEN_1312366_1312454_1412185 N, BT1N28.PHONGBAN_1312366_1312454_1412185 P, BT1N28.DUAN_1312366_1312454_1412185 D,BT1N28.CHINHANH_1312366_1312454_1412185 CN
     where (N.MANV=P.TRUONGPHONG or N.MANV=D.TRUONGDA or N.MANV=CN.TRUONGCHINHANH) and N.MANV=sys_context('userenv','SESSION_USER');
     if (flag=0) then
       dbms_session.set_context('NhanVien_context','KiemTraNhanVienKhongGiuChucVu',1);
@@ -30,8 +31,8 @@ as
     dbms_session.set_context('NhanVien_context','MaPhong','P00');
     select maPhong
     into MP
-    from BT1N28.NHANVIEN_366_454_185
-    where BT1N28.NHANVIEN_366_454_185.MANV=sys_context('userenv','SESSION_USER');
+    from BT1N28.NHANVIEN_1312366_1312454_1412185
+    where BT1N28.NHANVIEN_1312366_1312454_1412185.MANV=sys_context('userenv','SESSION_USER');
 
     dbms_session.set_context('NhanVien_context','MaPhong',MP);
   end;
@@ -61,11 +62,12 @@ as
   as
   predicate varchar(2000);
   begin
-    if(sys_context('NhanVien_context','MaPhong') ='P00') then
+    if((sys_context('NhanVien_context','MaPhong') ='P00') or 
+      (sys_context('NhanVien_context','KiemTraNhanVienKhongGiuChucVu')=2)) then
       return '1=1';
     end if;
     if(sys_context('NhanVien_context','KiemTraNhanVienKhongGiuChucVu')=1) then
-      predicate:='NHANVIEN_366_454_185.maphong=sys_context(''NhanVien_context'',''MaPhong'')';
+      predicate:='NHANVIEN_1312366_1312454_1412185.maphong=sys_context(''NhanVien_context'',''MaPhong'')';
       return predicate;
     else return '1=1';
     end if;
@@ -75,15 +77,21 @@ as
   as
   predicate varchar(2000);
   begin
+    if((sys_context('NhanVien_context','MaPhong') ='P00') or 
+      (sys_context('NhanVien_context','KiemTraNhanVienKhongGiuChucVu')=2)) then
+      return '1=1';
+    end if;
+  
     if(sys_context('NhanVien_context','KiemTraNhanVienKhongGiuChucVu')=1) then
-      predicate:='NHANVIEN_366_454_185.MaNV=sys_context(''userenv'',''SESSION_USER'')';
+      predicate:='NHANVIEN_1312366_1312454_1412185.MaNV=sys_context(''userenv'',''SESSION_USER'')';
       return predicate;
     else return '1=1';
     end if; 
   end;
 end;
   
-execute DBMS_RLS.ADD_POLICY(OBJECT_SCHEMA => 'BT1N28',OBJECT_NAME => 'NHANVIEN_366_454_185',POLICY_NAME => 'ThongTinNhanVien_Policy_pak' ,FUNCTION_SCHEMA => 'BT1N28' ,POLICY_FUNCTION => 'ThongTinNhanVien_Policy_pak.ThongTinCaNhan_Policy',STATEMENT_TYPES => 'select', POLICY_TYPE =>DBMS_RLS.DYNAMIC);
-execute DBMS_RLS.ADD_POLICY(OBJECT_SCHEMA => 'BT1N28',OBJECT_NAME => 'NHANVIEN_366_454_185',POLICY_NAME => 'ThongTinLuong_Policy' ,FUNCTION_SCHEMA => 'BT1N28' ,POLICY_FUNCTION => 'ThongTinNhanVien_Policy_pak.ThongTinLuong_Policy',STATEMENT_TYPES => 'select' ,sec_relevant_cols=>'Luong',sec_relevant_cols_opt=>dbms_rls.ALL_ROWS);
---execute dbms_rls.drop_policy(OBJECT_SCHEMA => 'BT1N28',OBJECT_NAME => 'NHANVIEN_366_454_185',POLICY_NAME => 'ThongTinNhanVien_Policy_pak' )
+execute DBMS_RLS.ADD_POLICY(OBJECT_SCHEMA => 'BT1N28',OBJECT_NAME => 'NHANVIEN_1312366_1312454_1412185',POLICY_NAME => 'ThongTinCaNhan_Policy' ,FUNCTION_SCHEMA => 'BT1N28' ,POLICY_FUNCTION => 'ThongTinNhanVien_Policy_pak.ThongTinCaNhan_Policy',STATEMENT_TYPES => 'select', POLICY_TYPE =>DBMS_RLS.DYNAMIC);
+execute DBMS_RLS.ADD_POLICY(OBJECT_SCHEMA => 'BT1N28',OBJECT_NAME => 'NHANVIEN_1312366_1312454_1412185',POLICY_NAME => 'ThongTinLuong_Policy' ,FUNCTION_SCHEMA => 'BT1N28' ,POLICY_FUNCTION => 'ThongTinNhanVien_Policy_pak.ThongTinLuong_Policy',STATEMENT_TYPES => 'select' ,sec_relevant_cols=>'Luong',sec_relevant_cols_opt=>dbms_rls.ALL_ROWS);
+--execute dbms_rls.drop_policy(OBJECT_SCHEMA => 'BT1N28',OBJECT_NAME => 'NHANVIEN_1312366_1312454_1412185',POLICY_NAME => 'ThongTinNhanVien_Policy_pak' )
+--execute dbms_rls.drop_policy(OBJECT_SCHEMA => 'BT1N28',OBJECT_NAME => 'NHANVIEN_1312366_1312454_1412185',POLICY_NAME => 'ThongTinLuong_Policy' )
 

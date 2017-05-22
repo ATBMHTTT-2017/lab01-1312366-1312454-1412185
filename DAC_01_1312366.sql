7610cb2d7c9247ae7244c0cbe6263750dbbc776f
tnminh <minhtn@hotmail.com.vn>
2017-05-22 22:30:48 +0700

    update

--- DAC_01_1312366.sql
+++ DAC_01_1312366.sql
@@ -1,4 +1,6 @@
 --
-grant select,insert,update on BT1N28.DuAn_366_454_185 to truong_phong;
-grant select on NHANVIEN_366_454_185 to truong_phong;
-grant select on NHANVIEN_366_454_185 to nhan_vien;
+grant select,insert,update on BT1N28.DuAn_1312366_1312454_1412185 to truong_phong;
+grant select on NHANVIEN_1312366_1312454_1412185 to truong_phong;
+grant select on NHANVIEN_1312366_1312454_1412185 to nhan_vien;
+grant select on NHANVIEN_1312366_1312454_1412185 to truong_du_an;
+grant select on NHANVIEN_1312366_1312454_1412185 to truong_chi_nhanh;
